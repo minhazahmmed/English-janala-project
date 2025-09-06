1a1f25fb663b7aa656ca9adb1a0cf9d660d0b280
minhazahmmed <minhazahmmed231@gmail.com>
2025-09-06 17:05:04 +0600

    word details added dynamically

--- script.js
+++ script.js
@@ -1,19 +1,103 @@
 
 
+
+const createElements = (arr) => {
+    const htmlElements = arr.map((el) => `<span class="btn">${el}</span>`);
+   return (htmlElements.join(" "));
+};
+
+const manageSpinner = (status) => {
+    if(status === true){
+        document.getElementById("spinner").classList.remove("hidden");
+        document.getElementById("word-container").classList.add("hidden");
+    }
+    else {
+        document.getElementById("word-container").classList.remove("hidden");
+        document.getElementById("spinner").classList.add("hidden");
+    }
+}
+
 const loadLessons = () => {
     fetch("https://openapi.programming-hero.com/api/levels/all")
     .then(res => res.json())
     .then(json => displayLessons(json.data)); 
 };
 
+const removeActive = () => {
+    const lessonButtons = document.querySelectorAll(".lesson-btn")
+    lessonButtons.forEach(btn => {
+        btn.classList.remove("active");
+    })
+}
+
+
 const loadLevelWord = (id) => {
-    
+    manageSpinner(true);
     const url = `https://openapi.programming-hero.com/api/level/${id}`
     fetch(url)
     .then(res => res.json())
-    .then(data => displayLevelWord(data.data))
+    .then(data => {
+        removeActive();
+        const clickBtn = document.getElementById(`lesson-btn-${id}`)
+        
+        clickBtn.classList.add("active")
+
+        displayLevelWord(data.data)
+    })
 }
 
+const loadWordDetail = async (id) => {
+ const url = `https://openapi.programming-hero.com/api/word/${id}`;
+ const res = await fetch(url);
+ const details = await res.json();
+ displayWordDetails(details.data);
+
+ }
+
+//  ta": {
+// "word": "Tranquil",
+// "meaning": "শান্ত / নিরিবিলি",
+// "pronunciation": "ট্রাঙ্কুইল",
+// "level": 6,
+// "sentence": "The park was a tranquil place to relax.",
+// "points": 4,
+// "partsOfSpeech": "adjective",
+// "synonyms": [
+// "peaceful",
+// "calm",
+// "serene"
+// ],
+
+const displayWordDetails = (word) => {
+    console.log(word);
+    const detailsBox = document.getElementById("detials-container");
+    detailsBox.innerHTML = `
+       <div>
+        <h2 class="text-2xl font-bold"> ${word.word} (<i class="fa-solid fa-microphone-lines"></i>:${word.pronunciation})</h2>
+      </div>
+
+      <div class="">
+        <h2 class="font-bold">Meaning</h2>
+        <p class="font-bangla">${word.meaning}</p>
+      </div>
+
+       <div class="">
+        <h2 class="font-bold">Example</h2>
+        <p>${word.sentence}</p>
+      </div>
+
+      <div class="">
+        <h2 class="font-bold">সমার্থক শব্দ গুলো</h2>
+        <div class="">${createElements(word.synonyms)}</div>
+      </div>
+    
+    
+    
+    `;
+
+    document.getElementById("word_modal").showModal();
+    
+}
 
 const displayLevelWord = (words) => {
     const wordContainer = document.getElementById("word-container");
@@ -27,15 +111,10 @@ const displayLevelWord = (words) => {
         <h2 class="text-4xl">নেক্সট Lesson এ যান</h2>
     </div>
        
-       `
+       `;
+       manageSpinner(false);
+       return;
     }
-
-
-//     "id": 4,
-// "level": 5,
-// "word": "Diligent",
-// "meaning": "পরিশ্রমী",
-// "pronunciation": "ডিলিজেন্ট"
 
     words.forEach(word => {
         const wordDiv = document.createElement("div")
@@ -46,7 +125,9 @@ const displayLevelWord = (words) => {
         <p class="text-[20px] font-medium">Meaning /Pronounciation</p>
         <div class="text-[32px] font-semibold text-[#18181B] font-bangla ">"${word.meaning ? word.meaning : "Meaning not found"} / ${word.pronunciation ? word.pronunciation : "Pronunciation not found"}"</div>
         <div class="flex justify-between">
-            <button class="px-4 py-3 bg-[#1A91FF10] cursor-pointer rounded-lg hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
+
+            <button onclick="loadWordDetail(${word.id})" class="px-4 py-3 bg-[#1A91FF10] cursor-pointer rounded-lg hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
+
             <button class="px-4 py-3 bg-[#1A91FF10] cursor-pointer rounded-lg hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
         </div>
     </div>
@@ -54,9 +135,9 @@ const displayLevelWord = (words) => {
         `
         wordContainer.appendChild(wordDiv)
     })
+
+    manageSpinner(false);
 }
-
-
 
 
 const displayLessons = (lessons) => {
@@ -68,7 +149,7 @@ const displayLessons = (lessons) => {
     const btnDiv = document.createElement("div");
     btnDiv.innerHTML = `
 
-    <button onclick = "loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary"><i class="fa-solid fa-book-open"></i> Lesson-${lesson.level_no}</button>
+    <button id="lesson-btn-${lesson.level_no}" onclick = "loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary lesson-btn"><i class="fa-solid fa-book-open"></i> Lesson-${lesson.level_no}</button>
 
     `
     levelContainer.appendChild(btnDiv)
